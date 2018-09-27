4750961b8f64367606a3042957ba71cfa4cfb2d8
EDUARDO MENDOZA <rl238513@rlas-macair-36.rlas-116.org>
2018-09-27 11:07:20 -0500

    added an if then statement with variables

--- OperatorsChallenge/ViewController.swift
+++ OperatorsChallenge/ViewController.swift
@@ -12,13 +12,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if numberThree < numberTwo {
+            numberOne + numberTwo
+        }
+        else { numberOne - numberTwo }
+        
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var firstNumber: UILabel!
+    var numberOne = 4
+    
+    @IBOutlet weak var secondNumber: UILabel!
+    var numberTwo = 9
+    
+    @IBOutlet weak var thirdNumber: UILabel!
+    var numberThree = 5
+    
+    @IBAction func calculateFormula(_ sender: Any) {
     }
+
+
+
+
 
 
 }
